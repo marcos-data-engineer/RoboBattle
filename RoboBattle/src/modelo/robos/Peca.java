3044aa5bc028feeede3368f82ed3238fa2490b1a
marcosvandrade <vascorj2006@gmail.com>
2019-06-23 17:48:22 -0300

    commitHome23Jun19

--- RoboBattle/src/modelo/robos/Peca.java
+++ RoboBattle/src/modelo/robos/Peca.java
@@ -6,13 +6,17 @@ public class Peca {
 	private int defesa;
 	private int velocidade;
 	private int durabilidade;
+	private int resistencia;
+	private int poder;
 	
-	public Peca(int ataque, int defesa, int velocidade, int durabilidade) {
+	public Peca(int ataque, int defesa, int velocidade, int durabilidade, int resistencia, int poder) {
 		super();
 		this.ataque = ataque;
 		this.defesa = defesa;
 		this.velocidade = velocidade;
 		this.durabilidade = durabilidade;
+		this.resistencia = resistencia;
+		this.poder = poder;
 	}
 
 	public int getAtaque() {
@@ -57,9 +61,31 @@ public class Peca {
 		this.durabilidade = durabilidade;
 	}
 
+	public int getResistencia(){
+		if(this.funciona()){
+			return resistencia;
+		}
+		return 0;
+	}
+
+	public void setResistencia(int resistencia) {
+		this.resistencia = resistencia;
+	}
+
+	public int getPoder(){
+		if(this.funciona()){
+			return poder;
+		}
+		return 0;
+	}
+
+	public void setPoder(int poder) {
+		this.poder = poder;
+	}
+
 	@Override
 	public String toString() {
-		return "Peça [A=" + ataque + ", D=" + defesa + ", Dur="+ durabilidade +"]";
+		return "Peça [A=" + ataque + ", D=" + defesa + ", Dur="+ durabilidade +", R="+ resistencia+",P="+poder+"]";
 	}
 	
 	public boolean funciona() {
